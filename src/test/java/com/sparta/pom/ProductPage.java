e170dfacb68e0c37cf238adff74017770d95c692
josef <josef.abdulkarim@hotmail.co.uk>
2026-03-30 15:58:24 +0100

    added accepting consent method

--- src/test/java/com/sparta/pom/ProductPage.java
+++ src/test/java/com/sparta/pom/ProductPage.java
@@ -42,6 +42,9 @@ import org.openqa.selenium.support.FindBy;
            @FindBy(css = ".product-information p:nth-of-type(4)")
            private WebElementFacade brand;
 
+           @FindBy(css = "button[aria-label='Consent']")
+           private WebElementFacade consentButton;
+
            public ProductPage(WebDriver driver) {
            }
 
@@ -77,6 +80,17 @@ import org.openqa.selenium.support.FindBy;
                        && availability.isVisible()
                        && condition.isVisible()
                        && brand.isVisible();
+           }
+
+           public void acceptConsent() {
+               try {
+                   if (consentButton.isVisible()) {
+                       consentButton.waitUntilClickable().click();
+                       consentButton.waitUntilNotVisible();
+                   }
+               } catch (Exception e) {
+                   System.out.println(e);
+               }
            }
        }
 
