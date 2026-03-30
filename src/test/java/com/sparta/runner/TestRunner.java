69fc10d747a90edde0e69353c661058b55439839
kevin musisi <musisi709@gmail.com>
2026-03-30 16:21:59 +0100

    sign up page

--- src/test/java/com/sparta/runner/TestRunner.java
+++ src/test/java/com/sparta/runner/TestRunner.java
@@ -1,45 +1,46 @@
 package com.sparta.runner;
 
 import com.sparta.pom.SignupPage;
+import com.sparta.pom.AccountDetailsPage;
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
-import net.thucydides.core.annotations.Managed;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 public class TestRunner {
 
         @Managed
+        WebDriver driver;
 
+        @Steps
         SignupPage signupPage;
 
+        @Steps
+        AccountDetailsPage detailsPage;
+
         @Test
-        @DisplayName("Test the full signup process from start to finish")
-        public void testFullSignup() {
+        public void testCompleteRegistrationFlow() {
+                // PAGE 1: Initial Signup
                 signupPage.open();
+                String uniqueEmail = "sparta" + System.currentTimeMillis() + "@test.com";
+                signupPage.enterNameAndEmail("Spartan User", uniqueEmail);
+                signupPage.clickSignup();
 
-
-                String uniqueEmail = "sparta_tester" + System.currentTimeMillis() + "@test.com";
-
-
-                signupPage.enterInitialSignup("Kevin", uniqueEmail);
-
-
-                signupPage.completeFullForm(
-                        "Sparta123!",       // Password
-                        "15",               // Day
-                        "April",            // Month (Must be full name)
-                        "1995",             // Year
-                        "Kevin",            // First Name
-                        "Tester",           // Last Name
-                        "123 Sparta Way",   // Address
-                        "United States",    // Country
-                        "New York",         // State
-                        "New York City",    // City
-                        "10001",            // Zip Code
-                        "07123456789"       // Mobile Number
+                // PAGE 2: Detailed Information
+                detailsPage.fillAccountInfo("SecurePass123", "10", "July", "1995");
+                detailsPage.fillAddressInfo(
+                        "John", "Doe", "123 High St",
+                        "India", "Maharashtra", "Mumbai", "400001", "9876543210"
                 );
+                detailsPage.clickCreateAccount();
+
+                // Final Assertion
+                assertThat(driver.getCurrentUrl(), containsString("account_created"));
         }
 }
