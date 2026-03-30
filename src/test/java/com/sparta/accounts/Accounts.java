ba41d4bd040f97afc024d618ebb1921261f1f8cb
josef <josef.abdulkarim@hotmail.co.uk>
2026-03-30 18:40:34 +0100

    corrected account creator

--- src/test/java/com/sparta/accounts/Accounts.java
+++ src/test/java/com/sparta/accounts/Accounts.java
@@ -1,47 +1,63 @@
 package com.sparta.accounts;
 
 public class Accounts {
-    private static final String VALID_EMAIL = "testUser@exampleEmail.co.uk";
+    private static final String VALID_EMAIL = "@exampleEmail.co.uk";
     private static final String VALID_PASSWORD = "password456123";
 
-    private static final Account account1 = new Account(
+    public static final Account account1 = new Account(
             "theLegend@example.com",
             "passwordLegends1999",
-            "3-5-1989",
+            "3",
+            "May",
+            "1989",
             "Tom",
             "Legend",
             "15 example road",
-            "united testers",
+            "United States",
+            "California",
+            "Malibu",
             "nw1 1xx",
             "07429583981"
     );
 
-    private static final Account account2 = new Account(
+    public static final Account account2 = new Account(
             "theSecond@example.com",
             "imaginePass123",
-            "3-5-1989",
+            "3",
+            "May",
+            "1989",
             "Rowan",
             "Sparky",
             "45 the other road",
-            "Republic of Automations",
+            "India",
+            "Maharashtra",
+            "Mumbai",
             "S45 4xx",
             "07837584381"
     );
 
-    private static final Account account3 = new Account(
+    public static final Account account3 = new Account(
             "third@example.com",
             "Expasswordple8921",
-            "13-11-2001",
+            "13",
+            "November",
+            "2001",
             "Spockle",
             "Vonny",
             "55 Back Alley",
-            "Great Britain",
+            "Canada",
+            "Ontario",
+            "Toronto",
             "N18 3x0",
             "07845829012"
     );
 
-    public record Account(String email, String password, String DOB, String firstName, String lastName, String address,
-                          String country, String cityZipCode, String mobileNumber) {
+    public static String getValidEmail() {
+        return "test" + System.currentTimeMillis() + VALID_EMAIL;
+    }
+
+    public record Account(String email, String password, String DOB_day, String DOB_month, String DOB_year, String firstName, String lastName, String address,
+                          String country, String state, String city, String cityZipCode, String mobileNumber) {
 
         public String getName() {
                 return firstName + " " + lastName;
