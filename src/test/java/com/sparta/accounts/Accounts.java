38be21262327536f92a252e88c7aeb6ba31a15d5
josef <josef.abdulkarim@hotmail.co.uk>
2026-03-30 15:31:53 +0100

    Created Accounts to ease the use of ready-made accounts for testing.

---
+++ src/test/java/com/sparta/accounts/Accounts.java
@@ -0,0 +1,39 @@
+package com.sparta.accounts;
+
+public class Accounts {
+    private static final String VALID_EMAIL = "testUser@exampleEmail.co.uk";
+    private static final String VALID_PASSWORD = "password456123";
+
+    private static final Account account1 = new Account(
+            "theLegend@example.com",
+            "passwordLegends1999",
+            "3-5-1989",
+            "Tom",
+            "Legend",
+            "15 example road",
+            "united testers",
+            "nw1 1xx",
+            "07429583981"
+    );
+
+    private static final Account account2 = new Account(
+            "theSecond@example.com",
+            "imaginePass123",
+            "3-5-1989",
+            "Rowan",
+            "Sparky",
+            "45 the other road",
+            "Republic of Automations",
+            "S45 4xx",
+            "07837584381"
+    );
+
+    public record Account(String email, String password, String DOB, String firstName, String lastName, String address,
+                          String country, String cityZipCode, String mobileNumber) {
+
+        public String getName() {
+                return firstName + " " + lastName;
+            }
+
+        }
+}
