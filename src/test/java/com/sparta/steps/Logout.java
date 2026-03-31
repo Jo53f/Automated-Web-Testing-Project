eb4a1cd674a437ade3e6e795f4087044facaa8f9
josef <josef.abdulkarim@hotmail.co.uk>
2026-03-31 14:07:30 +0100

    Created logout cucumber and skeleton steps class

---
+++ src/test/java/com/sparta/steps/Logout.java
@@ -0,0 +1,12 @@
+package com.sparta.steps;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+
+public class Logout {
+    @And("I am signed in")
+    public void iAmSignedIn() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
