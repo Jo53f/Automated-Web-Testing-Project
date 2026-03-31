667c477e6d65b98684a2559b6fcb45d20340a09f
josef <josef.abdulkarim@hotmail.co.uk>
2026-03-31 09:20:26 +0100

    added steps for cucumber, logging in and navigating to login page.

---
+++ src/test/java/com/sparta/steps/NavigateToLoginSteps.java
@@ -0,0 +1,33 @@
+package com.sparta.steps;
+
+import com.sparta.pom.Homepage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import net.thucydides.core.annotations.Managed;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
+
+public class NavigateToLoginSteps {
+
+    @Managed
+    Homepage homepage;
+
+    @Given("I am on the homepage")
+    public void iAmOnTheHomePage() {
+        homepage.open();
+    }
+
+    @And("I have clicked Signup / Login")
+    public void iHaveClickedOnSignUpAndLogin() {
+        homepage.clickOnLoginSignUp();
+    }
+
+    @Then("I should land on the Signup / Login page")
+    public void iShouldLandOnTheInventoryPage() {
+        String url = getDriver().getCurrentUrl();
+        MatcherAssert.assertThat(url, Matchers.is("https://automationexercise.com/login"));
+    }
+}
