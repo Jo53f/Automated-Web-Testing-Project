24ff5ea8d255aca1a01f1fc13b58019ff4a75a3b
josef <josef.abdulkarim@hotmail.co.uk>
2026-03-31 15:46:34 +0100

    temporary changes to ease merging

--- src/test/java/com/sparta/steps/AutoExTest.java
+++ src/test/java/com/sparta/steps/AutoExTest.java
@@ -1,7 +1,7 @@
 package com.sparta.steps;
 
 import com.sparta.pom.*;
-//import com.sparta.pom.ProductsPage;
+//import com.sparta.pom.ProductsPagetemp;
 //import com.sparta.pom.CartPage;
 import net.thucydides.core.annotations.Managed;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
@@ -19,17 +19,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AutoExTest {
 
     @Managed
-    HomePage home;
+    HomePagetemp home;
     //HPage home;
 
     @Managed
-    ProductsPage products;
+    ProductsPagetemp products;
 
 
 
 
     @Managed
-    LoginSignup login;
+    LoginSignuptemp login;
 
     @Managed
     CartPage checkout;
